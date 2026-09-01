0c6be9f6174cac7b179845f7c724e946cda42ed6
Alex Williamson <85185426+alex-williamson96@users.noreply.github.com>
2026-08-31 17:03:26 -0700

    Add: time-based-key-value-store - submission-5

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-5.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+
+class TimeMap:
+
+    def __init__(self):
+        self.time_map = defaultdict(list)
+        
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.time_map[key].append((value, timestamp))
+
+    def get(self, key: str, timestamp: int) -> str:
+        values = self.time_map[key]
+
+        if not values:
+            return ""
+        
+        left = 0
+        right = len(values) - 1
+
+        while left <= right:
+            # largest previous or equal timestamp
+            mid = (left + right) // 2
+            t = values[mid][1]
+
+            if t > timestamp:
+                right = mid - 1
+            else:
+                left = mid + 1
+        if right < 0:
+            return ""
+        return values[right][0]
